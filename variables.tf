e77d2775a5d4d540c3798de1669ed5323a5ac51e
christine otuo-acheampong <chrisotuo@yahoo.co.uk>
2025-06-18 23:10:41 -0400

    first terraform

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    description = "this is the region resorces wikk be created"
+    type = string
+    default = "us-east-1"
+}
+
+variable "bucket_name" {
+    description = "name of bucket to create"
+    type = string
+    default = "chrisotuoA" 
+}
